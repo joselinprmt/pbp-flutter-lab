3b8734c84d7cb1cca5febdea2ba48ef639217fc9
Joselin Permata Aprillia <103546691+joselinprmt@users.noreply.github.com>
2022-11-08 22:27:40 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Program Counter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Program Counter'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _counter % 2 == 0
+                ? const Text(
+                    "GENAP",
+                    style: TextStyle(
+                      color: Colors.red,
+                    ),
+                  )
+                : const Text(
+                    "GANJIL",
+                    style: TextStyle(
+                      color: Colors.blue,
+                    ),
+                  ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(left: 30),
+        child: Row(crossAxisAlignment: CrossAxisAlignment.end, children: [
+          Visibility(
+            visible: _counter < 1 ? false : true,
+            child: FloatingActionButton(
+              // Decrement button
+              onPressed: _decrementCounter,
+              tooltip: 'Decrement',
+              child: const Icon(Icons.remove),
+            ),
+          ),
+          Expanded(
+            child: Container(),
+          ),
+          FloatingActionButton(
+            // Increament button
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+        ]),
+      ),
+    );
+  }
+}
